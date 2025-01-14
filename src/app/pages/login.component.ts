884e1dd1998c1a888d083ff9d1f9d2a0969e2640
Robert Ngo <robertncl@gmail.com>
2025-01-14 15:00:51 +0800

    add login component

---
+++ src/app/pages/login.component.ts
@@ -0,0 +1,18 @@
+@Component({
+  selector: 'app-login',
+  template: `
+      <div class="login-container">
+          <h1>Login</h1>
+          <form>
+              <input type="text" name="email" placeholder="Email">
+              <input type="password" name="password"
+                  placeholder="Password">
+              <button type="submit">Login</button>
+          </form>
+      </div>
+  `,
+  standalone: true,
+})
+export class LoginComponent {
+  credentials = { email: '', password: '' };
+}
